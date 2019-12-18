2fef1cb330132f0798b8b9154744794c3eebd673
henrygenus <henry.a.genus@gmail.com>
2019-12-17 20:23:35 -0800

    removed unnecessary script

--- lab4/compile_b.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-string=`uname` 
-if [[ $string == *"beaglebone"* ]]; then
-    gcc lab4b_src/lab4b.c lab4b_src/main_b.c $@ -lmraa -lm -o lab4b
-else
-    gcc -DDUMMY $@ lab4b_src/debug.c lab4b_src/lab4b.c lab4b_src/main_b.c -lm -o lab4b
-fi 
-
